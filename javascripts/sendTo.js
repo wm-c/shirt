cca18e6d6c77480ce8052fba34cda3f8277dec05
William <Wcmeathrel@gmail.com>
2020-02-24 23:06:59 -0500

    Changes

--- javascripts/sendTo.js
+++ javascripts/sendTo.js
@@ -2,9 +2,9 @@ function postToGoogle() {
     var text = $('#textContainer').val();
     
     $.ajax({
-      url: "https://docs.google.com/forms/u/1/d/e/1FAIpQLScvWGXg4CvGfyA2fbzPpDJnzhLtFzhL1ImhGerH-iyeOLMefw/formResponse",
+      url: "https://docs.google.com/forms/u/1/d/e/1FAIpQLSfKePSLaSTuXDx6iKv4yESu3clU1f7Cf2eoTxdEUxUuN9doNQ/formResponse",
       data: {
-         "entry.36277984": text
+         "entry.970992871": text
       },
       type: "POST",
       dataType: "xml",
